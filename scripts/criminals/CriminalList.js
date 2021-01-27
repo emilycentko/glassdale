7c18f633d1f1913d28b65eb5ce6162e0cf93aea4
emilycentko <emily.centko@gmail.com>
2021-01-27 15:39:07 -0600

    Filter criminals by arresting officers dropdown

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -57,3 +57,20 @@ eventHub.addEventListener("crimeChosen", crimeChosenEvent => {
         renderToDom(filteredCriminalsArray)
     }
 })
+
+eventHub.addEventListener("officerSelected", officerChosenEvent => {
+    // How can you access the officer name that was selected by the user?
+    const officerName = officerChosenEvent.detail.officer
+
+    // How can you get the criminals that were arrested by that officer?
+    const criminals = useCriminals()
+    const filteredCriminalsArray = criminals.filter(
+        criminalObject => {
+            if (criminalObject.arrestingOfficer === officerName) {
+                return true
+            }
+        }
+    )
+
+    renderToDom(filteredCriminalsArray)
+})
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import { ConvictionSelect } from "./convictions/ConvictionSelect.js"
 import { CriminalList } from "./criminals/CriminalList.js"
+import { OfficerSelect } from "./officers/OfficerSelect.js"
 
 CriminalList()
 ConvictionSelect()
+OfficerSelect()
